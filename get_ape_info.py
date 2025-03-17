1014ae4766ba8d9695053572228fbfd82f073071
ecollin2 <ecollin@seas.upenn.edu>
2025-03-17 12:42:37 -0600

    Update get_ape_info.py

--- get_ape_info.py
+++ get_ape_info.py
@@ -15,7 +15,7 @@ with open('ape_abi.json', 'r') as f:
 
 ############################
 # Connect to an Ethereum node
-api_url = ""  # YOU WILL NEED TO PROVIDE THE URL OF AN ETHEREUM NODE
+api_url = "https://mainnet.infura.io/v3/e27000ffe194432f9edd0830736528d9"  # YOU WILL NEED TO PROVIDE THE URL OF AN ETHEREUM NODE
 provider = HTTPProvider(api_url)
 web3 = Web3(provider)
 
@@ -28,6 +28,32 @@ def get_ape_info(ape_id):
     data = {'owner': "", 'image': "", 'eyes': ""}
 
     # YOUR CODE HERE
+    try: 
+        owner_address = contract.functions.ownerOf(ape_id).call()
+        data["owner"] = owner_address 
+
+        metadata_uri = contract.functions.tokenURI(ape_id).call()
+
+        if metadata_uri.startswith("ipfs://"):
+            ipfs_gateway = "https://gateway.pinata.cloud/ipfs/"
+            metadata_url = metadata_uri.replace("ipfs://", ipfs_gateway)
+        else: 
+            metadata_url=metadata_uri
+
+        metadata_response = requests.get(metadata_url)
+        metadata=metadata_response.json()
+
+        data["image"] = metadata.get("image", "N/A")
+
+        eye_trait = next(
+            (trait["value"] for trait in metadata.get("attributes", []) if trait.get("trait_type")== "Eyes"), 
+            "Unknown"
+        )
+        data["eyes"] = eye_trait 
+
+    except Exception as error: 
+        print(f"Error retrieving data {ape_ID}: {e}")
+    
 
     assert isinstance(data, dict), f'get_ape_info{ape_id} should return a dict'
     assert all([a in data.keys() for a in
